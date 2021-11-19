b659e5e6d7c410f40053d1b9e15bbf699db5ad33
Shinichi Takayanagi <shinichi.takayanagi@gmail.com>
2021-11-19 15:55:51 +0900

    Update

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from sudachipy import tokenizer, dictionary
 st.text_input("なんか文章を入力してくれぃ", key="value")
 
 dic = dictionary.Dictionary(dict_type="full").create()
-words = [m.surface() for m in dic.tokenize(st.session_state.value, tokenizer.Tokenizer.SplitMode.C)]
 
-st.caption("検索システム内部ではこういう風に分割されるよ！")
-for word in words:
-    st.write(word)
+st.caption("検索システム内部ではこういう風に名刺だけ分割・抽出されるよ！")
+is_noun = lambda x: x.part_of_speech()[0] == "名詞"
+for morph in dic.tokenize(st.session_state.value, tokenizer.Tokenizer.SplitMode.C):
+    if not is_noun(morph):
+        continue
+    st.write(morph.surface())
