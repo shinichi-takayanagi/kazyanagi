18ba3775dc634c414b9b59d4b1afb6a971250d12
Shinichi Takayanagi <shinichi.takayanagi@gmail.com>
2021-11-19 15:24:32 +0900

    Update

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from sudachipy import tokenizer, dictionary
 
 st.text_input("なんか文章を入力してくれぃ", key="value")
 
-tokenizer = dictionary.Dictionary(dict_type="full").create()
-words = [m.surface() for m in tokenizer.tokenize(st.session_state.value, tokenizer.Tokenizer.SplitMode.C)]
+dic = dictionary.Dictionary(dict_type="full").create()
+words = [m.surface() for m in dic.tokenize(st.session_state.value, tokenizer.Tokenizer.SplitMode.C)]
 
 st.caption("検索システム内部ではこういう風に分割されるよ！")
 for word in words:
